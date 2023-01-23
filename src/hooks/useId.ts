9f38943b601c31bab9bc0ef91b083304d030ca1e
Steve Rydz <steve.rydz@canonical.com>
2023-01-23 14:28:23 +0000

    Create CardList component

--- src/hooks/useId.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from "react";
-import { nanoid } from "nanoid";
-
-/**
- * A hook that returns the same random ID string on every render.
- * Can be used as a value for HTML "id" attributes.
- * @returns random ID string
- */
-export const useId = (): string => useRef(nanoid()).current;
