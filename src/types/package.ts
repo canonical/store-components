da6b7c8e8f7cd21985b834c3b4ba9351410d14cd
Steve Rydz <steve.rydz@canonical.com>
2023-02-02 15:00:26 +0000

    Add PackageCard tests

--- src/types/package.ts
+++ src/types/package.ts
@@ -1,23 +1,26 @@
+const PackageType = ["snap", "charm", "bundle", "interface"];
+const ValidationType = ["star", "verified", undefined];
+
 export type Package = {
   package: {
-    description?: string;
-    display_name?: string;
+    description: string;
+    display_name: string;
     icon_url?: string;
-    name?: string;
+    name: string;
     platforms?: Array<string> | null;
-    type?: "snap" | "charm" | "bundle" | "interface";
+    type: typeof PackageType[number];
     charms?: Array<{
-      name?: string;
-      display_name?: string;
-    }> | null;
+      name: string;
+      display_name: string;
+    }>;
   };
-  publisher: {
+  publisher?: {
     display_name?: string;
     name?: string;
-    validation?: "star" | "verified" | null;
+    validation?: typeof ValidationType[number];
   };
-  categories: Array<{
-    display_name?: string;
-    name?: string;
+  categories?: Array<{
+    display_name: string;
+    name: string;
   }>;
 };
