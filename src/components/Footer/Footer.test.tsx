935ab29eaeb274dd6667e59f1db9a47a05d2409d
Seulkee Kang <seulkeekang@gmail.com>
2023-02-13 18:10:58 +0000

    modified Footer.tsx and Footer.stories.mdx files

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -4,8 +4,12 @@ import React from "react";
 import Footer from "./Footer";
 
 describe("Footer", () => {
+  const props = {
+    socialLinks: [],
+  };
+
   it("renders", () => {
-    render(<Footer />);
+    render(<Footer {...props} />);
     expect(true).toBe(true);
   });
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 import { Row, Col, Strip } from "@canonical/react-components";
 
-function Footer() {
+type SocialLink = {
+  iconUrl: string;
+  href: string;
+  altText: string;
+};
+
+export type Props = {
+  socialLinks: SocialLink[];
+};
+
+function Footer({ socialLinks }: Props) {
   return (
     <Strip type="dark">
       <Row className="u-equal-height">
         <Col size={3}>
           <p>© {new Date().getFullYear()} Canonical Ltd.</p>
+          <ul className="p-inline-list">
+            {socialLinks.map((socialLink) => (
+              <li key={socialLink.href}>
+                <a href={socialLink.href}>
+                  <img src={socialLink.iconUrl} alt={socialLink.altText} />
+                </a>
+              </li>
+            ))}
+          </ul>
         </Col>
       </Row>
     </Strip>
