c0083a9187846c8a7004263bfd81e1f58901d0bc
Seulkee Kang <seulkeekang@gmail.com>
2023-02-14 17:32:09 +0000

    made social links configurable

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -4,8 +4,25 @@ import React from "react";
 import Footer from "./Footer";
 
 describe("Footer", () => {
+  const props = {
+    socialLinks: [
+      {
+        name: "Twitter",
+        iconUrl: "https://example.com/icon1.png",
+        href: "https://example.com/link1",
+        altText: "Link 1",
+      },
+      {
+        name: "Github",
+        iconUrl: "https://example.com/icon2.png",
+        href: "https://example.com/link2",
+        altText: "Link 2",
+      },
+    ],
+  };
+
   it("renders", () => {
-    render(<Footer />);
+    render(<Footer {...props} />);
     expect(true).toBe(true);
   });
 });
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Row, Col, Strip } from "@canonical/react-components";
 
-function Footer() {
+import "./Footer.scss";
+
+type SocialLink = {
+  name: string;
+  iconUrl: string;
+  href: string;
+  altText: string;
+};
+
+export type Props = {
+  socialLinks: SocialLink[];
+};
+
+function Footer({ socialLinks }: Props) {
   return (
-    <Strip type="dark">
+    <Strip type="light">
       <Row className="u-equal-height">
-        <Col size={3}>
+        <Col size={4}>
           <p>© {new Date().getFullYear()} Canonical Ltd.</p>
+          <ul className="p-inline-list">
+            {socialLinks &&
+              socialLinks.map((socialLink) => (
+                <li className="p-inline-list__item" key={socialLink?.name}>
+                  <a href={socialLink?.href}>
+                    <img
+                      src={socialLink?.iconUrl}
+                      width={24}
+                      height={24}
+                      alt={socialLink?.altText}
+                    />
+                  </a>
+                </li>
+              ))}
+          </ul>
         </Col>
       </Row>
     </Strip>
