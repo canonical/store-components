de246e1027b0b7a5e76f4c4b064f78f84c6059c7
Steve Rydz <steve.rydz@canonical.com>
2023-03-23 10:16:42 +0000

    Remove pagination from CardList component

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
-import { Pagination, Row, Col } from "@canonical/react-components";
+import React from "react";
+import { Row, Col } from "@canonical/react-components";
 
 import PackageCard from "components/PackageCard";
 import type { Package as PackageProps } from "../../types/package";
 
 export type Props = {
   packages: Array<PackageProps>;
-  itemsPerPage: number;
 };
 
-function CardList({ packages, itemsPerPage }: Props) {
-  const [currentPageNumber, setCurrentPageNumber] = useState(1);
-  const [packagesToShow, setPackagesToShow] = useState(
-    packages.slice(0, itemsPerPage)
-  );
-
+function CardList({ packages }: Props) {
   return (
     <>
       <Row>
-        {packagesToShow.length > 0 &&
-          packagesToShow.map((item) => (
+        {packages.length > 0 &&
+          packages.map((item) => (
             <Col
               size={3}
               key={item?.package?.name}
@@ -29,25 +23,6 @@ function CardList({ packages, itemsPerPage }: Props) {
             </Col>
           ))}
       </Row>
-      <div className="u-fixed-width">
-        <Pagination
-          itemsPerPage={itemsPerPage}
-          totalItems={packages.length}
-          paginate={(pageNumber) => {
-            const index = pageNumber - 1;
-            setCurrentPageNumber(pageNumber);
-            setPackagesToShow(
-              packages.slice(
-                itemsPerPage * index,
-                itemsPerPage * index + itemsPerPage
-              )
-            );
-          }}
-          currentPage={currentPageNumber}
-          scrollToTop
-          centered
-        />
-      </div>
     </>
   );
 }
