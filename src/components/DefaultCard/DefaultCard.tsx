7e7794293e54e47eaf09b494d1292f37694d4c15
Steve Rydz <steve.rydz@canonical.com>
2023-03-28 15:18:21 +0100

    Remove ratings from default card

--- src/components/DefaultCard/DefaultCard.tsx
+++ src/components/DefaultCard/DefaultCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import PackageCard from "components/PackageCard";
 
 function DefaultCard({ data }) {
-  return <PackageCard data={data} showIcon showRatings showVerification />;
+  return <PackageCard data={data} showIcon showVerification />;
 }
 
 export default DefaultCard;
