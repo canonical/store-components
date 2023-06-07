cc6aee041f38bf75e55e23f23e8db97b4a466fff
Seulkee Kang <seulkeekang@gmail.com>
2023-06-07 16:15:14 +0100

    made entire package cards clickable

--- src/components/PackageCard/IconCard.tsx
+++ src/components/PackageCard/IconCard.tsx
@@ -6,14 +6,16 @@ function IconCard({ iconUrl, displayName, children }) {
       className="p-media-object u-no-margin--bottom"
       style={{ height: "100%" }}
     >
-      <img
-        src={iconUrl}
-        width={48}
-        height={48}
-        alt=""
-        className="p-media-object__image"
-        data-testid="package-icon"
-      />
+      <a href={`/${displayName}`}>
+        <img
+          src={iconUrl}
+          width={48}
+          height={48}
+          alt=""
+          className="p-media-object__image"
+          data-testid="package-icon"
+        />
+      </a>
       <div
         className="sc-package-card p-media-object__details"
         style={{ width: "100%" }}
