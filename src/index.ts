df88e939b8e38ac8ce4020d95d10ab6ca714e4a7
Steve Rydz <steve.rydz@canonical.com>
2023-06-15 13:35:24 +0100

    Export loading card component

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { default as DefaultCard } from "./components/DefaultCard";
 export { default as Filters } from "./components/Filters";
 export { default as Footer } from "./components/Footer";
 export { default as InterfaceCard } from "./components/InterfaceCard";
+export { default as LoadingCard } from "./components/LoadingCard";
 
 export type {
   ClassName,
