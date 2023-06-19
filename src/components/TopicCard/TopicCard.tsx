cc2baf40b029aa71c214398ee8394013a1fc4109
Steve Rydz <steve.rydz@canonical.com>
2023-06-19 16:40:31 +0100

    Add overflow to topic cards (#54)

--- src/components/TopicCard/TopicCard.tsx
+++ src/components/TopicCard/TopicCard.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { Card } from "@canonical/react-components";
 
-function TopicCard({ data }) {
+import "./TopicCard.scss";
+
+type Props = {
+  data: {
+    slug: string;
+    name: string;
+    description: string;
+  };
+  truncateTitle?: boolean;
+  truncateContent?: boolean;
+};
+
+function TopicCard({ data, truncateTitle, truncateContent }: Props) {
   return (
     <Card
       style={{ borderTop: "3px solid #f2a031" }}
@@ -9,9 +21,13 @@ function TopicCard({ data }) {
         window.location.href = `/topics/${data.slug}`;
       }}
     >
-      <h3 className="p-muted-heading">{data.name}</h3>
+      <h3 className={`p-muted-heading ${truncateTitle ? "u-truncate" : ""}`}>
+        {data.name}
+      </h3>
       <hr />
-      <p>{data.description}</p>
+      <p className={truncateContent ? "u-line-clamp" : ""}>
+        {data.description}
+      </p>
     </Card>
   );
 }
