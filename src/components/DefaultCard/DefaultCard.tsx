3e46ea38b0a4e30acc5c2d473448f24d5708c6a6
Steve Rydz <steve.rydz@canonical.com>
2023-12-04 14:19:19 +0000

    Add featured card

--- src/components/DefaultCard/DefaultCard.tsx
+++ src/components/DefaultCard/DefaultCard.tsx
@@ -3,7 +3,18 @@ import React from "react";
 import PackageCard from "components/PackageCard";
 
 function DefaultCard({ data }) {
-  return <PackageCard data={data} showIcon showVerification />;
+  const isFeatured = data?.categories.find((cat) => {
+    return cat.featured;
+  });
+
+  return (
+    <PackageCard
+      data={data}
+      showIcon
+      showVerification
+      isHighlighted={isFeatured || false}
+    />
+  );
 }
 
 export default DefaultCard;
